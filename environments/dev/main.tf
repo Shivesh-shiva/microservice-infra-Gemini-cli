7aa49f6348c936ad2b5c22edc7ee598cc213003f
Shivesh-shiva <shiveshec1032@gmail.com>
2026-06-06 00:04:18 +0530

    Import existing resource group into Terraform state

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,6 +3,11 @@ module "resource_groups" {
   resource_groups = var.resource_groups
 }
 
+import {
+  to = module.resource_groups.azurerm_resource_group.this["rg-microservices-dev"]
+  id = "/subscriptions/${var.ARM_SUBSCRIPTION_ID}/resourceGroups/rg-microservices-dev"
+}
+
 module "acr" {
   source     = "../../modules/acr"
   registries = var.registries
